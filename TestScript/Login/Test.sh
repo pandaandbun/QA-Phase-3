953d02827628519be865f9f868e5c434669fb5e2
trantienanhta <trantienanh.ta@protonmail.com>
2021-03-01 19:43:18 -0500

    FEAT & FIX - test script, login tests and improvement

--- TestScript/Login/Test.sh
+++ TestScript/Login/Test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Run Login Test Scripts
 
+# Clear the log
+> Test_Result.txt
+
+# Run make
+# make -f ../../Makefile || exit
+find ../../ -type f -name Makefile -execdir make \;
+
 # Loop through all test folders
 for folder_name in */; do
     # Test number and name
-    test_number=${folder_name:0:1}
+    test_number=${folder_name%/}
     test_name=login$test_number
 
     # Input, Output, Transaction
@@ -13,8 +20,34 @@ for folder_name in */; do
     etf=$test_name.etf
 
     # Print test name
-    echo "Run test " ${folder_name:0:1}
+    echo "Run test " $test_number
 
     # Run test
-    ../../bank < $folder_name/Expected/$inp > $folder_name/Ran/$bto
+    ../../bank ../../currentbankaccountfile.txt $test_number/Ran/$etf < $test_number/Expected/$inp > $test_number/Ran/$bto
+
+    # Compare results
+    # Actual outputs
+    actual_etf=$test_number/Ran/$etf
+    actual_bto=$test_number/Ran/$bto
+    # Expected Outputs
+    expected_etf=$test_number/Expected/$etf
+    expected_bto=$test_number/Expected/$bto
+
+    echo "Run test " ${folder_name:0:1} >> Test_Result.txt
+
+    # ETF differences
+    echo "ETF"
+    diff $actual_etf $expected_etf
+
+    echo "ETF" >> Test_Result.txt
+    diff $actual_etf $expected_etf >> Test_Result.txt
+
+    # BTO differences
+    echo "BTO"
+    diff $actual_bto $expected_bto
+
+    echo "BTO" >> Test_Result.txt
+    diff $actual_bto $expected_bto >> Test_Result.txt
+
+    echo ""  >> Test_Result.txt
 done
